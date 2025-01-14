32510e6ebe408b3a724594f6dec6bc7b4e701b6e
Maryam Nasir <maryamnasir555@yahoo.com>
2025-01-14 16:49:40 +0500

    Create Root Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
           defaultTheme="system"
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <h1 className="text-2xl font-black text-white">
-      Welcome to the world of Next.js
-    </h1>
-  );
-};
-
-export default Home;
