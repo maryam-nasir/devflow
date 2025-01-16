53a5cb31b560909d76c8d608d41d6b356edc7910
Maryam Nasir <maryamnasir555@yahoo.com>
2025-01-16 10:53:41 +0500

    Add Sign out button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,6 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = () => {
   return (
@@ -17,17 +12,17 @@ const Home = () => {
       <h1 className="h1-bold font-space-grotesk">
         Welcome to the world of Next.js (Inter)
       </h1>
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <form
+        className="px-4 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({
+            redirectTo: ROUTES.SIGN_IN,
+          });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
     </>
   );
 };
