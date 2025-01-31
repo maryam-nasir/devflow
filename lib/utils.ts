68149549cf3149a26a66a30d9e99c7f2f238fe58
Maryam Nasir <maryamnasir555@yahoo.com>
2025-01-31 16:38:19 +0500

    Added Devicons for tags

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { techIconMap } from "@/constants/techIconMap";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getDeviconClassName = (technology: string) => {
+  const techName = technology.replace(/[ .]/g, "").toLowerCase();
+
+  return techIconMap[techName]
+    ? `${techIconMap[techName]} colored`
+    : "devicon-devicon-plain";
+};
