2caed9a32499e6dd43b378932d326a71767bfcac
Maryam Nasir <maryamnasir555@yahoo.com>
2025-02-07 20:23:52 +0500

    Implement Home filters

---
+++ components/filters/HomeFilter.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+import { formUrlQuery, removeKeysFromUrlQuery } from "@/lib/url";
+import { cn } from "@/lib/utils";
+
+import { Button } from "../ui/button";
+
+const filters = [
+  { name: "React", value: "react" },
+  { name: "JavaScript", value: "javascript" },
+  // { name: "Newest", value: "newest" },
+  //   { name: "Popular", value: "popular" },
+  //   { name: "Unanswered", value: "unanswered" },
+  //   { name: "Recommended", value: "recommended" },
+];
+
+const HomeFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const filterParam = searchParams.get("filter") || "";
+
+  const [active, setActive] = useState<string>(filterParam);
+
+  const handleSelectFilter = (filterValue: string) => {
+    let newUrl;
+    if (filterValue === active) {
+      setActive("");
+      newUrl = removeKeysFromUrlQuery({
+        params: searchParams.toString(),
+        keys: ["filter"],
+      });
+    } else {
+      setActive(filterValue);
+      newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: filterValue,
+      });
+    }
+    router.push(newUrl, { scroll: false });
+  };
+
+  return (
+    <div className="mt-10 hidden flex-wrap gap-3 md:flex">
+      {filters.map((filter) => (
+        <Button
+          key={filter.name}
+          className={cn(
+            `body-medium rounded-lg px-6 py-3 capitalize shadow-none`,
+            active === filter.value
+              ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
+              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+          )}
+          onClick={() => handleSelectFilter(filter.value)}
+        >
+          {filter.name}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default HomeFilter;
